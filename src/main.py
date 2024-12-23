a48e207254ccfc5928a0725013aa6815717e4e8a
Zachary Smith <128327411+HydeZero@users.noreply.github.com>
2024-12-22 20:10:29 -0600

    Revert "Forget it, lets use flutter"
    This reverts commit dd86c880fa1155fcbb6274597687c2142acd3325.

---
+++ src/main.py
@@ -0,0 +1,33 @@
+import subprocess
+import os
+import time
+
+username = os.environ["HOME"].split("/")[-1] # usually the last item is the username so grab it. This assumes linux, since flatpak runs on linux only.
+
+print(username) # debug
+
+def installApp(flatpakApp):
+    try:
+        subprocess.run(["flatpak", "install", flatpakApp, "--assumeyes"])
+    except Exception as e:
+        print(f"Error installing {flatpakApp}. Details:\n", e)
+
+try:
+    os.mkdir(f"/home/{username}/.flatpaktransfer") # attempt to make a directory for the files to be transferred to
+except:
+    print("Directory already exists.")
+
+current_files = os.listdir(f"/home/{username}/.flatpaktransfer") # initial setting of current files
+
+def waitForFiles():
+    while True:
+        prev_files = current_files
+        current_files = os.listdir(f"/home/{username}/.flatpaktransfer")
+        for file in current_files:
+            if file not in prev_files:
+                if file.split(".")[-1] == "flatpakref": # detect if it is a flatpakref file
+                    installApp(f"/home/{username}/.flatpaktransfer/{file}") # Absolute directory to allow running program in any pwd
+        time.sleep(1) # sleep for a second to prevent excessive assignment of variables
+
+print("Initializing...")
+
